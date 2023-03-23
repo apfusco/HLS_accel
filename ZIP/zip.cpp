f884aa3050de996254d944fe4dd936222870bac7
Alexander Fusco <afusco1@arizona.edu>
2023-03-23 12:06:02 -0700

    ZIP: Fix sizes in for-loops

--- ZIP/zip.cpp
+++ ZIP/zip.cpp
@@ -18,7 +18,7 @@ void zip_add_top(ZIP_T A[MAX_SIZE],
     // Calculate matrix multiplication using local data buffer based on input size
     // and write results into local buffer for C
 zip_add_l:
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < 2 * size; i++) {
 #pragma HLS LOOP_TRIPCOUNT min = 2 max = MAX_SIZE
         C[i] = A[i] + B[i];
     }
@@ -32,7 +32,7 @@ void zip_sub_top(ZIP_T A[MAX_SIZE],
     // Calculate matrix multiplication using local data buffer based on input size
     // and write results into local buffer for C
 zip_sub_l:
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < 2 * size; i++) {
 #pragma HLS LOOP_TRIPCOUNT min = 2 max = MAX_SIZE
         C[i] = A[i] - B[i];
     }
@@ -46,7 +46,7 @@ void zip_div_top(ZIP_T A[MAX_SIZE],
     // Calculate matrix multiplication using local data buffer based on input size
     // and write results into local buffer for C
 zip_div_l:
-    for (int i = 0; i < size; i += 2) {
+    for (int i = 0; i < 2 * size; i += 2) {
 #pragma HLS LOOP_TRIPCOUNT min = 2 max = MAX_SIZE
         C[i    ] = ( A[i] * B[i    ] + A[i + 1] * B[i + 1]) / (B[i] * B[i] + B[i + 1] * B[i + 1]);
         C[i + 1] = (-A[i] * B[i + 1] + A[i + 1] * B[i    ]) / (B[i] * B[i] + B[i + 1] * B[i + 1]);
@@ -61,7 +61,7 @@ void zip_mult_top(ZIP_T A[MAX_SIZE],
     // Calculate matrix multiplication using local data buffer based on input size
     // and write results into local buffer for C
 zip_mult_l:
-    for (int i = 0; i < size; i=i+2) {
+    for (int i = 0; i < 2 * size; i=i+2) {
 #pragma HLS LOOP_TRIPCOUNT min = 2 max = MAX_SIZE
         C[i] = A[i] * B[i] - A[i+1] * B[i+1];
         C[i+1] = A[i] * B[i+1] + A[i+1] * B[i];
